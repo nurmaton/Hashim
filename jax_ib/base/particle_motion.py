ee972786f8747af66726c3df211b32490986cf96
abdulhamid <94214939+nurmaton@users.noreply.github.com>
2025-07-28 14:12:45 +0300

    Update particle_motion.py

--- jax_ib/base/particle_motion.py
+++ jax_ib/base/particle_motion.py
@@ -1,20 +1,22 @@
 from jax_ib.base import particle_class as pc
 from jax_ib.base import interpolation
-from jax_ib.base import IBM_Force # Import the whole module
+from jax_ib.base import IBM_Force
 import jax
 import jax.numpy as jnp
 
-# --- NEW FUNCTION to update the massive, deformable particle ---
 def update_massive_deformable_particle(all_variables, dt, gravity_g=0.0):
-    particles = all_variables.particles
+    """
+    Updates the state of a massive, deformable particle for one time step.
+    This version is simplified for a single particle to be JAX-compatible.
+    """
+    particles_container = all_variables.particles
     velocity_field = all_variables.velocity
     
-    # We will update the first particle in the list. This can be extended to a loop.
-    particle = particles.particles[0] 
+    # Directly access the single particle from its container.
+    particle = particles_container.particles[0] 
     
     # --- 1. Advect Fluid Markers (X) with the local fluid velocity ---
-    # Interpolate fluid velocity to the fluid markers X.
-    # This is still a simplification, a fully vectorized interpolation would be ideal.
+    # Using jax.vmap for this interpolation loop is JAX-compatible.
     U_fluid_x_pts = jax.vmap(interpolation.point_interpolation, in_axes=(0, None))(jnp.stack([particle.xp, particle.yp], axis=1), velocity_field[0].array)
     U_fluid_y_pts = jax.vmap(interpolation.point_interpolation, in_axes=(0, None))(jnp.stack([particle.xp, particle.yp], axis=1), velocity_field[1].array)
     
@@ -22,13 +24,11 @@ def update_massive_deformable_particle(all_variables, dt, gravity_g=0.0):
     new_yp = particle.yp + dt * U_fluid_y_pts
 
     # --- 2. Update Mass Markers (Y) using Newton's Second Law (Eq. 5) ---
-    # F_i^m = Kp(Y-X)
     penalty_force_x, penalty_force_y = IBM_Force.calculate_penalty_force(
         particle.xp, particle.yp, particle.Ym_x, particle.Ym_y, particle.stiffness
     )
 
-    # Net force on mass markers: F_net_Y = -F_i^m - Mg
-    # Assuming gravity acts in the negative y-direction (ẑ in the paper)
+    # Net force on mass markers: F_net_Y = -F_penalty - Mg
     net_force_on_Ym_x = -penalty_force_x
     net_force_on_Ym_y = -penalty_force_y - (particle.mass_per_marker * gravity_g)
 
@@ -36,11 +36,9 @@ def update_massive_deformable_particle(all_variables, dt, gravity_g=0.0):
     accel_x = net_force_on_Ym_x / particle.mass_per_marker
     accel_y = net_force_on_Ym_y / particle.mass_per_marker
 
-    # Update velocity of mass markers (Velocity Verlet)
+    # Update velocity and position of mass markers
     new_Vm_x = particle.Vm_x + dt * accel_x
     new_Vm_y = particle.Vm_y + dt * accel_y
-    
-    # Update position of mass markers
     new_Ym_x = particle.Ym_x + dt * new_Vm_x
     new_Ym_y = particle.Ym_y + dt * new_Vm_y
     
@@ -48,18 +46,18 @@ def update_massive_deformable_particle(all_variables, dt, gravity_g=0.0):
     new_center = jnp.array([[jnp.mean(new_Ym_x), jnp.mean(new_Ym_y)]])
 
     # --- 3. Create the new particle object with the updated state ---
-    updated_particle = particle_class.particle(
+    updated_particle = pc.particle(
         xp=new_xp, yp=new_yp, Ym_x=new_Ym_x, Ym_y=new_Ym_y,
         Vm_x=new_Vm_x, Vm_y=new_Vm_y,
         mass_per_marker=particle.mass_per_marker, stiffness=particle.stiffness, sigma=particle.sigma,
         particle_center=new_center, geometry_param=particle.geometry_param,
-        Grid=particle.Grid, shape=particle.shape
+        Grid=particle.Grid, shape=particle.shape,
+        # Pass dummy kinematic functions to satisfy the constructor, as they are no longer used.
+        Displacement_EQ=particle.Displacement_EQ, Rotation_EQ=particle.Rotation_EQ
     )
     
-    # Update the list of particles in the main state object
-    # In a multi-particle simulation, this would be a loop or a vmap.
-    updated_particles_list = particles.particles.copy()
-    updated_particles_list[0] = updated_particle
-    new_particles_container = particles.tree_replace(particles=updated_particles_list)
+    # --- 4. Rebuild the container directly with the updated particle ---
+    # This avoids Python list operations which are not JAX-compatible.
+    new_particles_container = pc.particle_lista(particles=[updated_particle])
     
     return all_variables.tree_replace(particles=new_particles_container, Step_count=all_variables.Step_count + 1)
